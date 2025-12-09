f13f9b47fb6cd7bbbed85c3406b7b323ac2c3bd3
Aakarshan Singh <aakarshan221003@gmail.com>
2025-12-09 19:39:25 +0000

    feat: added ssl cert and key

--- grpc-backend/server.go
+++ grpc-backend/server.go
@@ -6,6 +6,7 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	pb "simplegrpcserver/proto/gen"
 )
@@ -21,13 +22,22 @@ func (s *server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddReponse, e
 }
 
 func main() {
+
+	cert := "certs/cert.pem"
+	key := "certs/key.pem"
+
 	port := ":50051"
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatal("Failed to listen:", err)
 	}
 
-	grpcServer := grpc.NewServer()
+	creds, err := credentials.NewServerTLSFromFile(cert, key)
+	if err != nil {
+		log.Fatal("Error loading cert:", err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 
 	pb.RegisterCalculateServer(grpcServer, &server{})
 
